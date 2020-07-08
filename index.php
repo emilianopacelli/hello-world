d068c05d3b931700446652a9648fa3b83216f84d
Emiliano Pacelli <52998606+emilianopacelli@users.noreply.github.com>
2020-07-08 18:57:11 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+ <head>
+  <title>OCP Hello World</title>
+ </head>
+ <body>
+ <?php echo '<h2 style="color:#E25648";>Hello OpenShift!</h2>'; ?> 
+ <?php $nextWeek = time() + (7 * 24 * 60 * 60);
+   echo 'Today is:       '. date('Y-m-d') ."\n"; ?>
+ </body>
+</html>
